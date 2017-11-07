ebf0baa9b08ea45a005cef66414c6cc963ddf2c3
Jonathan <jonathan@ubots.com.br>
2017-11-07 13:56:18 -0200

    adding calls: get cards and get cards actions

--- src/main/kotlin/br/com/Main.kt
+++ src/main/kotlin/br/com/Main.kt
@@ -7,7 +7,15 @@ object Main {
 
     @JvmStatic
     fun main(args : Array<String>){
-        val userInfo = br.com.trello.TrelloServiceClient(RestTemplate(), args[1], args[3]).getUserInfo()
+        // Exemplos de chamadas para métricas do trello
+        val restTemplate = RestTemplate()
+
+        val userInfo = TrelloServiceClient(restTemplate, args[1], args[3]).getUserInfo()
+        val cardActions = TrelloServiceClient(restTemplate, args[1], args[3]).getCardActions(args[5])
+        val boardInfo = TrelloServiceClient(restTemplate, args[1], args[3]).getBoardInfo(args[7])
+
         println(userInfo)
+        println(cardActions)
+        println(boardInfo)
     }
 }
--- src/main/kotlin/br/com/trello/TrelloServiceClient.kt
+++ src/main/kotlin/br/com/trello/TrelloServiceClient.kt
@@ -1,14 +1,20 @@
 package br.com.trello
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.RequestEntity
 import org.springframework.web.client.RestTemplate
+import java.net.URI
 import java.util.*
 
 class TrelloServiceClient(var restClient: RestTemplate,
                           var apiKey: String,
                           var token: String) {
 
-     fun getUserInfo(): Optional<User>{
+    fun getUserInfo(): Optional<User>{
         try {
             val response = restClient.getForEntity("https://api.trello.com/1/members/me?fields=all&key=${apiKey}&token=${token}", User::class.java)
             return Optional.of(response.body)
@@ -18,7 +24,38 @@ class TrelloServiceClient(var restClient: RestTemplate,
         }
     }
 
+    fun getCardActions(cardId: String): Optional<List<CardAction>>{
+        try {
+            val request = RequestEntity<Any>(HttpMethod.GET, URI.create("https://api.trello.com/1/cards/${cardId}/actions?fields=all&key=${apiKey}&token=${token}"))
+            val respType = object: ParameterizedTypeReference<List<CardAction>>(){}
+
+            val response = restClient.exchange(request, respType)
+            return Optional.of(response.body)
+        }
+        catch (ex: org.jsoup.HttpStatusException){
+            return Optional.empty<List<CardAction>>()
+        }
+    }
+
+    fun getBoardInfo(cardId: String): Optional<List<Card>>{
+        try {
+            val request = RequestEntity<Any>(HttpMethod.GET, URI.create("https://api.trello.com/1/boards/${cardId}/cards?fields=all&key=${apiKey}&token=${token}"))
+            val respType = object: ParameterizedTypeReference<List<Card>>(){}
+
+            val response = restClient.exchange(request, respType)
+            return Optional.of(response.body)
+        }
+        catch (ex: org.jsoup.HttpStatusException){
+            return Optional.empty<List<Card>>()
+        }
+    }
 }
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class User constructor (var fullName: String, var idBoards: Array<String>)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Card constructor (var id: String, var idList: String, var name: String)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class CardAction constructor (var id: String, var type: String, var date: Date)
