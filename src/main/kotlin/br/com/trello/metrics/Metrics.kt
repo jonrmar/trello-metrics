16f98c9a696fda6b1302bc3dc503d2fa4e6755ef
Jonathan <jonathan@ubots.com.br>
2017-11-07 00:48:06 -0200

    first commit

---
+++ src/main/kotlin/br/com/trello/metrics/Metrics.kt
@@ -0,0 +1,5 @@
+package br.com.trello.metrics
+
+interface Metrics {
+    fun calculate()
+}
---
+++ src/main/kotlin/br/com/trello/trello/TrelloServiceClient.kt
@@ -0,0 +1,24 @@
+package br.com.trello.trello
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import org.springframework.web.client.RestTemplate
+import java.util.*
+
+class TrelloServiceClient(var restClient: RestTemplate,
+                          var apiKey: String,
+                          var token: String) {
+
+     fun getUserInfo(): Optional<User>{
+        try {
+            val response = restClient.getForEntity("https://api.trello.com/1/members/me?fields=all&key=${apiKey}&token=${token}", User::class.java)
+            return Optional.of(response.body)
+        }
+        catch (ex: org.jsoup.HttpStatusException){
+            return Optional.empty<User>()
+        }
+    }
+
+}
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class User constructor (var fullName: String, var idBoards: Array<String>)
